da6caf7b69eb13af410d47030ba9b42933d242bb
謝博丞 <96316508+hsiehbocheng@users.noreply.github.com>
2023-09-04 13:26:54 +0800

    add root path to solve yt_concat package import error

--- yt_concat/main.py
+++ yt_concat/main.py
@@ -2,6 +2,7 @@ import json
 import urllib.request
 import sys
 import os
-sys.path.append('./')
+sys.path.append('./') # Add the root directory to the Python path so that we can import the yt_concat package
+
 from yt_concat.settings import YOUTUBE_API_KEY
 print(YOUTUBE_API_KEY)
