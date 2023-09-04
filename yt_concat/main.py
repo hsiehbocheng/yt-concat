82c243ec40324a909efb52cf83fa5022d5a1f4ef
謝博丞 <96316508+hsiehbocheng@users.noreply.github.com>
2023-09-04 12:20:48 +0800

    update

---
+++ yt_concat/main.py
@@ -0,0 +1,4 @@
+import json
+import urllib.request
+from yt_concat.settings import YOUTUBE_API_KEY
+print(YOUTUBE_API_KEY)
---
+++ yt_concat/settings.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
