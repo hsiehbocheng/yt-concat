3e18dc4ba790f31830b88c3eb0bd1cf8bbd70476
謝博丞 <96316508+hsiehbocheng@users.noreply.github.com>
2023-09-05 12:49:27 +0800

    add comment

--- yt_concat/pipeline/pipeline.py
+++ yt_concat/pipeline/pipeline.py
@@ -6,7 +6,7 @@ class Pipeline:
         self.steps = steps
     
     def run(self, inputs, utils):
-        data = None
+        data = None # data 初始值為 None, 因為一開始下載資料並不需要 data, 但後續會依序傳遞下去
         for step in self.steps:
             try:
                 data = step.process(inputs=inputs, data=data, utils=utils)
